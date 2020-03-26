d398ac88795ed86c9e35bdfa0b13340aaefe6fab
Alan Ashworth <awashworth927@gmail.com>
2020-03-26 18:03:53 -0400

    Modified code structure for ES6 syntax and Promise performance.

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
 var inquirer = require("inquirer");
 var axios = require("axios");
 var fs = require("fs");
+var util = require("util");
 
-inquirer
-  .prompt([
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUserForGitHubInfo = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "username",
       message: "What is your GitHub username?"
     }
-  ])
+  ]);
+};
+
+const promptUserForProjectInfo = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the name of the project?"
+    },
+    {
+      type: "input",
+      name: "projectDescription",
+      message: "Provide a description of the project:"
+    },
+    {
+      type: "input",
+      name: "projectUsage",
+      message: "How will this project be used?"
+    },
+    {
+      type: "input",
+      name: "projectLicense",
+      message: "Under what license with this project be protected?"
+    },
+    {
+      type: "input",
+      name: "projectContributors",
+      message: "Who is contributing to this project?"
+    }
+  ]);
+};
+
+promptUserForGitHubInfo()
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function(response) {
       const userEmail = response.data.email;
       const userAvatar = response.data.avatar_url;
-      // console.log(userEmail);
-      // console.log(userAvatar);
-      getProjectInfo();
+      promptUserForProjectInfo()
+        .then(data => {
+          console.log(data);
+
+          fs.writeFile("GENERATE-README.md", JSON.stringify(data), function(
+            error
+          ) {
+            if (error) {
+              throw error;
+            }
+            console.log("saved data");
+          });
+        })
+        .catch(error => {
+          console.log("Inquirer prompt error: ", error);
+        });
     });
   })
   .catch(error => {
     console.log("Inquirer prompt error: ", error);
   });
-
-const getProjectInfo = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "projectTitle",
-        message: "What is the name of the project?"
-      },
-      {
-        type: "input",
-        name: "projectDescription",
-        message: "Provide a description of the project:"
-      },
-      {
-        type: "input",
-        name: "projectUsage",
-        message: "How will this project be used?"
-      },
-      {
-        type: "input",
-        name: "projectLicense",
-        message: "Under what license with this project be protected?"
-      },
-      {
-        type: "input",
-        name: "projectContributors",
-        message: "Who is contributing to this project?"
-      }
-    ])
-    .then(data => {
-      console.log(data);
-
-      fs.writeFile("README.md", JSON.stringify(data), function(error) {
-        if (error) {
-          throw error;
-        }
-        console.log("saved data");
-      });
-    })
-    .catch(error => {
-      console.log("Inquirer prompt error: ", error);
-    });
-};
