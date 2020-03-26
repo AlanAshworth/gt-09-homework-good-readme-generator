b8e9ad313695d3a1539781220dc18e85d2f9d940
Alan Ashworth <awashworth927@gmail.com>
2020-03-25 23:10:08 -0400

    Add axios. Pull user information from GitHub with axios. Create method to collect basic information for readme generator.

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 var inquirer = require("inquirer");
+var axios = require("axios");
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
       type: "input",
-      message: "What is your GitHub username?",
-      name: "username"
+      name: "username",
+      message: "What is your GitHub username?"
     }
-  ]);
+  ])
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+
+    axios.get(queryUrl).then(function(response) {
+      const userEmail = response.data.email;
+      const userAvatar = response.data.avatar_url;
+      // console.log(userEmail);
+      // console.log(userAvatar);
+      getProjectInfo();
+    });
+  });
+
+const getProjectInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the name of the project?"
+      },
+      {
+        type: "input",
+        name: "projectDescription",
+        message: "Provide a description of the project:"
+      },
+      {
+        type: "input",
+        name: "projectUsage",
+        message: "How will this project be used?"
+      },
+      {
+        type: "input",
+        name: "projectLicense",
+        message: "Under what license with this project be protected?"
+      },
+      {
+        type: "input",
+        name: "projectContributors",
+        message: "Who is contributing to this project?"
+      },
+    ])
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log("Inquirer prompt error: ", error);
+    });
+};
